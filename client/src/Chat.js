4758209beac2e0fa4d73b42be17137e14f4f1f77
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-04-08 08:39:31 +0200

    feat(client/chat.js): use the Apollo cache for local state management

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useQuery, useMutation, useSubscription } from '@apollo/react-hooks';
 import {
 	addMessageMutation,
@@ -9,13 +9,15 @@ import MessageInput from './MessageInput';
 import MessageList from './MessageList';
 
 const Chat = ({ user }) => {
-	const [messages, setMessages] = useState([]);
-	useQuery(messagesQuery, {
-		onCompleted: ({ messages }) => setMessages(messages),
-	});
+	const { data } = useQuery(messagesQuery);
+	const messages = data ? data.messages : [];
 	useSubscription(messageAddedSubscription, {
-		onSubscriptionData: ({ subscriptionData }) => {
-			setMessages(messages.concat(subscriptionData.data.messageAdded));
+		onSubscriptionData: ({ client, subscriptionData }) => {
+			client.writeData({
+				data: {
+					messages: messages.concat(subscriptionData.data.messageAdded),
+				},
+			});
 		},
 	});
 	const [addMessage] = useMutation(addMessageMutation);
